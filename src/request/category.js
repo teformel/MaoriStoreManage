0edbed60086fb23c86c683a0b278316808acf9ca
maori <3182575061@qq.com>
2024-01-05 08:44:17 +0800

    feat: 紧急测试前版本

--- src/request/category.js
+++ src/request/category.js
@@ -25,3 +25,11 @@ export function deleteCategory(id) {
 export function getCategoryGoods(id) {
     return axios.get(`/admin/app_category_item/list?category_id=${id}`)
 }
+
+export function deleteCategoryGoods(id) {
+    return axios.post(`/admin/app_category_item/${id}/delete`)
+}
+
+export function connectCategoryGoods(data) {
+    return axios.post(`/admin/app_category_item`, data)
+}
